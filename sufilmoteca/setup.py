4fdb840715a28f325df24abe3224c915767e768f
manuel <manuel@debian>
2015-06-04 09:00:59 +0200

    cambio de mybotlle

---
+++ sufilmoteca/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Manuel',
+      version='1.0',
+      description='OpenShift App',
+      author='Manuel Chacon',
+      author_email='eb7bmg@gmail.com',
+      url='http://www.python.org/sigs/distutils-sig/',
+      install_requires=['bottle'],
+     )
---
+++ sufilmoteca/wsgi/mybottleapp.py
@@ -0,0 +1,32 @@
+#_*_ coding: utf-8 _*_
+from bottle import request, Bottle, route,run,template,static_file,default_app
+import requests 
+
+@route('/name/<name>')
+def nameindex(name='Stranger'):
+    return '<strong>Hola, %s!</strong>' % name
+ 
+@route('/')
+def index():
+    return '<strong>Hello World!</strong>'
+
+@route('/lista') # creamos una linea por cada informacion que saquemos del json
+def lista():
+	dicc={'pry_release_date.gte':'2014-09-15','primary_release_date.lte':'2014-10-22','api_key':'949149f8a7d1fe0fa6ac3eb641d11aae'}
+	url_base="http://api.themoviedb.org/3/"
+	r = requests.get(url_base+'discover/movie',params=dicc)
+	j=r.json()
+	lista=[]
+	for peli in j["results"]: # recorre las peliculas
+		lista.append(peli["title"]) # añade los titulos a que estaba vacia
+
+	return template("fichero.tpl", listado=lista) #retorna la lista
+
+# This must be added in order to do correct path lookups for the views
+import os
+from bottle import TEMPLATE_PATH
+TEMPLATE_PATH.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi/views/')) 
+
+application=default_app()
+
+
