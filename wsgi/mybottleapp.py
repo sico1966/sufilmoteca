bb703cd9d6d517e1af2637c88aeac70b2cdcab6a
manuel <manuel@debian>
2015-06-04 09:21:51 +0200

    cambio mybotlle

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -15,6 +15,11 @@ def lista():
 
 	return template("fichero.tpl", listado=lista) #retorna la lista
 
+# This must be added in order to do correct path lookups for the views
+import os
+from bottle import TEMPLATE_PATH
+TEMPLATE_PATH.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi/views/')) 
 
+application=default_app()
 
 
